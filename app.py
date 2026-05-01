585492aa3c08ea14a7781f20f682225df4aba714
fujirakucut-png <fujirakucut@gmail.com>
2026-05-01 22:24:33 +0900

    add requirements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,os
 import sqlite3
+import os
 
 app = Flask(__name__)
 DB = "inventory.db"
@@ -249,8 +250,6 @@ def logs():
 # 起動
 # =========================
 if __name__ == "__main__":
-    app.run(
-        debug=True,
-        host="0.0.0.0",
-        port=5000
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
