b02125a496acffa3ecf1b4513fb4bd52aabd5b5b
fujirakucut-png <fujirakucut@gmail.com>
2026-05-01 23:19:32 +0900

    remove temp db fix

--- app.py
+++ app.py
@@ -253,8 +253,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
-conn = sqlite3.connect(DB)
-conn.execute("ALTER TABLE stock_logs ADD COLUMN action TEXT")
-conn.commit()
-conn.close()    
+  
     
